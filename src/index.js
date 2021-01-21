f179dd9a31423c508576d927a96d8feffaec736d
Vityuk Yuriy <you.thunderstruck@gmail.com>
2021-01-22 00:02:29 +0200

    добавляет запоминание темы

--- src/index.js
+++ src/index.js
@@ -1,2 +1,40 @@
+import itemTemplates from '../src/gallery-item.hbs';
+import items from '../src/menu.json';
 import './styles.css';
 import './index.html';
+
+const menuRef = document.querySelector('.js-menu');
+const switcherButRef = document.querySelector('#theme-switch-toggle');
+const bodyRef = document.querySelector('body');
+
+const html = itemTemplates(items);
+menuRef.insertAdjacentHTML('beforeend', html);
+
+switcherButRef.addEventListener('change', () => {
+  if (bodyRef.classList.value === 'light-theme') {
+    dartThemeHandler();
+  } else {
+    lightThemeHandler();
+  }
+});
+
+const dartThemeHandler = function () {
+  bodyRef.classList.remove('light-theme');
+  bodyRef.classList.add('dark-theme');
+  switcherButRef.setAttribute('checked', true);
+  localStorage.setItem('theme', 'dark');
+  return;
+};
+const lightThemeHandler = () => {
+  bodyRef.classList.remove('dark-theme');
+  bodyRef.classList.add('light-theme');
+  switcherButRef.removeAttribute('checked');
+  localStorage.setItem('theme', 'light');
+  return;
+};
+
+if (localStorage.getItem('theme') === 'dark') {
+  dartThemeHandler();
+} else {
+  lightThemeHandler();
+}
